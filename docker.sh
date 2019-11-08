b43e0f29b66d17af180ae87df2f43516aac7fed5
NguyenHoaiPhuong <NguyenHoaiPhuong@github.com>
2019-11-08 23:35:55 +0700

    run docker without sudo

--- docker.sh
+++ docker.sh
@@ -25,3 +25,16 @@ sudo apt-get update
 
 # Install the latest version of Docker Engine - Community and containerd
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+############# Run Docker without sudo #############
+# Add the docker group if it doesn't already exist
+sudo groupadd docker
+
+# Add the connected user "$USER" to the docker group. Change the user name to match your preferred user if you do not want to use your current user
+sudo gpasswd -a $USER docker
+
+# Check docker by running below command
+docker run hello-world
+
+# in order to run docker, add below line into ~/.profile
+# export DOCKER_HOST=127.0.0.1:2375
