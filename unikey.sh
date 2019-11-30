5ba71ae04f8d1d00a430b75b84ee0b9c28273029
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2019-11-30 21:11:08 +0700

    Install unikey

---
+++ unikey.sh
@@ -0,0 +1,6 @@
+# References: https://vinasupport.com/huong-dan-cai-bo-go-tieng-viet-ibus-unikey-tren-ubuntu/
+
+sudo apt-get install ibus-unikey
+sudo add-apt-repository ppa:ubuntu-vn/ppa
+sudo apt-get update
+ibus restart
