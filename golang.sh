a6b733d0234130d8a97bd5b0423564b865615ede
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2019-11-30 21:11:00 +0700

    Install golang

---
+++ golang.sh
@@ -0,0 +1,5 @@
+sudo snap install --classic go
+
+echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export GOPATH=$HOME/go" >> ~/.profile
+echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.profile
