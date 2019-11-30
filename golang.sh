4adf5a529f901b71fb0f736b8fca41cac6b3cbf7
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2019-11-30 21:26:23 +0700

    Correct golang installation

--- golang.sh
+++ golang.sh
@@ -1,5 +1,5 @@
 sudo snap install --classic go
 
-echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export GOROOT=/snap/go" >> ~/.profile
 echo "export GOPATH=$HOME/go" >> ~/.profile
 echo "export PATH=$GOPATH/bin:$GOROOT/bin:$PATH" >> ~/.profile
