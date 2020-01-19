3d38aa564ad30f88ca16d0ca53e0110a3e99c83a
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-01-19 07:05:05 +0700

    Install hugo

---
+++ hugo.sh
@@ -0,0 +1,5 @@
+# Install the “extended” Sass/SCSS version with this command:
+snap install hugo --channel=extended
+
+# To install the non-extended version without Sass/SCSS support:
+# snap install hugo
