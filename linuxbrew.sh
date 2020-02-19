96f7a1202c00c0d07f1b38f2f2a98ea65912b930
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-02-19 17:13:19 +0700

    linux brew post-process

--- linuxbrew.sh
+++ linuxbrew.sh
@@ -1,4 +1,6 @@
 sudo apt install linuxbrew-wrapper
 
-# Test if it works
 # brew --help
+# echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.profile
+# echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.profile
+# echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.profile
