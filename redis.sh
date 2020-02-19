676fba7760d7801154aa2d6efa3932ec14b79ddb
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-02-19 14:31:54 +0700

    [redis] add reference link

--- redis.sh
+++ redis.sh
@@ -1,3 +1,5 @@
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-redis-on-ubuntu-18-04
+
 sudo apt-get update
 sudo apt-get install -y redis-server
 sudo systemctl start redis-server
