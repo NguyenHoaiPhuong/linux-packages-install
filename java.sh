59da68330089d033abc223bef3b48af49e12d696
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-03-04 12:06:30 +0700

    Install java

---
+++ java.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://linuxize.com/post/install-java-on-ubuntu-18-04/
+
+# echo 'First, update the apt package index with:'
+sudo apt update
+
+# echo 'Once the package index is updated install the default Java OpenJDK package with:'
+sudo apt install default-jdk
+
+# echo 'Verify the installation, by running the following command which will print the Java version:'
+java -version
