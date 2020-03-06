9d020f1f97976f10730e79514f22b2f8998e2469
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-03-06 09:36:03 +0700

    [java] Install java8

--- java.sh
+++ java.sh
@@ -6,7 +6,8 @@
 sudo apt update
 
 # echo 'Once the package index is updated install the default Java OpenJDK package with:'
-sudo apt install default-jdk
+# sudo apt install default-jdk
+sudo apt install openjdk-8-jdk
 
 # echo 'Verify the installation, by running the following command which will print the Java version:'
 java -version
