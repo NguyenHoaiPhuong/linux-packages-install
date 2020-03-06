509ee84ee123d49a5498fc550f860a9d9c4599f8
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2020-03-06 09:35:03 +0700

    Install inkscape for software design

---
+++ inkscape.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# http://ubuntuhandbook.org/index.php/2019/01/inkscape-0-92-4-released-install-ubuntu-18-04/
+# https://code.launchpad.net/~inkscape.dev/+archive/ubuntu/stable
+
+sudo add-apt-repository ppa:inkscape.dev/stable
+sudo apt-get update
+sudo apt-get install inkscape
